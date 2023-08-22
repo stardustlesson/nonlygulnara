894f9086b4c3b0f87400725b7fe9e4ddaf0b048e
Vaceslav <vaceslavchek667689@gmail.com>
2023-08-22 22:23:56 +0300

    Add style and update Calendar

--- hamb/celendar/ready-html/app/js/common.js
+++ hamb/celendar/ready-html/app/js/common.js
@@ -1,5 +1,18 @@
-$(function() {
+"use strict"
+function ChnageColor1() {
+	document.querySelector('body').classList.remove('violet', 'blue', 'skystyle');
+	document.querySelector('body').classList.add('red');
+}
+function ChnageColor2() {
+	document.querySelector('body').classList.remove('violet', 'red', 'skystyle');
+	document.querySelector('body').classList.add('blue');
+}
+function ChnageColor3() {
+	document.querySelector('body').classList.remove('red', 'blue', 'skystyle');
+	document.querySelector('body').classList.add('violet');
+}
+function ChnageColor4() {
+	document.querySelector('body').classList.remove('violet', 'blue', 'red');
+	document.querySelector('body').classList.add('skystyle');
+}
 
-	// Custom JS
-
-});
